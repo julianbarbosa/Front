d31425db310feebb39f5d28e470c40db0daf7a10
David Sandoval <dsanvoal0621@cali.gov.co>
2023-07-28 22:36:50 -0500

    Se actualizan vistas para mejorar la experiencia del usuario

--- bundles/PropiedadHorizontalBundle/ValidarPropiedadHorizontal/validarPropiedadHorizontal.js
+++ bundles/PropiedadHorizontalBundle/ValidarPropiedadHorizontal/validarPropiedadHorizontal.js
@@ -10,12 +10,13 @@ saul.config(["$stateProvider",
             }
         });
     }
-]).controller("validarPropiedadHorizontalCtrl", ["$scope", "root", "$http", "$stateParams", 
-    function ($scope, root, $http, $stateParams) {        
+]).controller("validarPropiedadHorizontalCtrl", ["$scope", "root", "$http", "$stateParams", "$window",
+    function ($scope, root, $http, $stateParams, $window) {        
         
         $(".overlap_espera").show();
         $(".overlap_espera_1").show();
-
+        $scope.nombrePropiedadHorizontal = '';
+        $scope.notificacion_radicado = '';
         $scope.idpropiedadhorizontal = $stateParams.idpropiedadhorizontal;
         console.log($scope.idpropiedadhorizontal);
 
@@ -24,8 +25,20 @@ saul.config(["$stateProvider",
                 console.log(data);
 
                 if (data.msg == "Ingreso correctamente") {
+                    $scope.numeroIdent = data.propiedadHorizontal['id'];
+                    $scope.tipoPersona = data.propiedadHorizontal['tipoPersona'];
                     $scope.nombrePropiedadHorizontal = data.propiedadHorizontal['nombre'];
                     $scope.direccionPropiedadHorizontal = data.propiedadHorizontal['direccion'];
+                    $scope.nombreRepresentanteLegal = data.propiedadHorizontal['nombreRepresentanteLegal'];
+                    $scope.representanteLegalFirma = data.propiedadHorizontal['representanteLegalFirma'];
+                    $scope.nitEmpresaAdministradora = data.propiedadHorizontal['nitEmpresaAdministradora'];
+                    $scope.cedulaRepresentanteLegal = data.propiedadHorizontal['cedulaRepresentanteLegal'];
+                    $scope.expedidaRepresentanteLegal = data.propiedadHorizontal['expedidaRepresentanteLegal'];
+                    $scope.resolucionPersoneriaJuridica = data.propiedadHorizontal['resolucionPersoneriaJuridica'];
+                    $scope.fechaResolucionPersoneriaJuridica = data.propiedadHorizontal['fechaResolucionPersoneriaJuridica'];
+                    $scope.personeriaJuridicaExpedidaPor = data.propiedadHorizontal['personeriaJuridicaExpedidaPor'];
+                    $scope.fechaResolucionRepresentanteLegal = data.propiedadHorizontal['fechaResolucionRepresentanteLegal'];
+                    $scope.periodoRepresentanteLegal = data.propiedadHorizontal['periodoRepresentanteLegal'];
                     console.log(data.propiedadHorizontal);
                     $scope.notificacion = '';
                 }
@@ -42,12 +55,39 @@ saul.config(["$stateProvider",
                 console.log(err);
             });
         
+        
 
-        // $scope.hideAlert = function () {
-        //     $scope.validator = {};
-        // };
+        // Funcion que inicia la solicitud de radicacion
+        $scope.solicitarCertificado = function ($numeroIdent, $tipoTramite) {
+            
+            if ($numeroIdent == undefined) {
+                alert("Debe consultar un propiedad horizontal para continuar");
+            }
+            else {
+                $(".overlap_espera").show();
+                $(".overlap_espera_1").show();
 
-        // $scope.cancel = function () {
-        // };
+                $http.post(root+"propiedadhorizontal/radicar_solicitud", {numeroIdent: $numeroIdent, tramite: $tipoTramite})
+                .success(function(data){
+                    console.log(data);
+                    
+                    
+                    if (data.error) {
+                        // Redirigir a la nueva URL
+                        window.location.href = '/saul/login_funcionario';
+                    }
+                    else if (data.data.numRadicado) {
+                        $scope.notificacion_radicado = 'Su solicitud ha sido radicada con el número '+data.data.numRadicado;
+                        //Desactiva el div de carga 
+                        $(".overlap_espera").fadeOut(500, "linear");
+                        $(".overlap_espera_1").fadeOut(500, "linear"); 
+                    }
+                })
+                .error(function(err){
+                    console.log(err);
+                });
+            }
+
+        };
     }
 ]);
